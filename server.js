e4b9dcf2101091fdcb9f2ad183e8f19d2baecd5b
srivarshithagatta <srivarshithagatta@gmail.com>
2025-10-15 08:59:42 +0530

    Y23_MERN_Render_backend'

---
+++ server.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv"; 
+dotenv.config(); 
+const app = express(); 
+const PORT = process.env.PORT || 8080; 
+// Middleware 
+app.use(cors()); 
+app.use(express.json()); 
+// Sample API route 
+app.get("/api/hello", (req, res) => { 
+res.json({ message: "Hello from Node + Express backend!" }); 
+}); 
+// Start server 
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
