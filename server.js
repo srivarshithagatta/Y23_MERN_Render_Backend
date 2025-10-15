ac3e15876245401c56d5ca5a81b0a55fbcbeedde
srivarshithagatta <srivarshithagatta@gmail.com>
2025-10-15 09:41:01 +0530

    change1

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv"; 
 dotenv.config(); 
+import express from "express";
+import cors from "cors";
 const app = express(); 
 const PORT = process.env.PORT || 8080; 
 // Middleware 
